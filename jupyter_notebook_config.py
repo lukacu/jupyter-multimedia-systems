92c4f09049346f5439233a6ae50fd88533f3eb38
Luka Čehovin <luka.cehovin@gmail.com>
2019-10-02 16:39:02 +0200

    Providing token from Hub?

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,6 @@
-c.NotebookApp.token=''
+import os
+
+c.NotebookApp.token=os.getenv("JPY_API_TOKEN", "")
 c.NotebookApp.password=''
 c.NotebookApp.terminals_enabled = False
 c.NotebookApp.notebook_dir = "/home/jovyan/work"
